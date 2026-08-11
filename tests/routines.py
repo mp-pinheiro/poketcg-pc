fa9e14d3ddc0c93f991381ad848f3c1d26c5bb7a
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-11 06:36:02 -0300

    feat(port): ir functions

--- tests/routines.py
+++ tests/routines.py
@@ -165,6 +165,7 @@ ROUTINES: dict[str, tuple[str, ...]] = {
     "load_deck": ("LoadDeck",),
     "hall_of_honor": ("HallOfHonorLoadMap",),
     "challenge_hall_lobby": ("Preload_ChallengeHallNPCs2",),
+    "ir_functions": ("PlayCardPopSong",),
 "card_color": ("GetCardWeakness", "GetArenaCardWeakness", "GetPlayAreaCardWeakness",
                "GetCardResistance", "GetArenaCardResistance", "GetPlayAreaCardResistance",
                "GetArenaCardColor", "GetPlayAreaCardColor", "HandleEnergyBurn"),
