c266085753c5292f9339b60a17b0f53172080398
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-11 07:00:26 -0300

    feat(port): debug main

---
+++ src/home/debug_main.h
@@ -0,0 +1,13 @@
+#ifndef POKETCG_HOME_DEBUG_MAIN_H
+#define POKETCG_HOME_DEBUG_MAIN_H
+
+#include <stdint.h>
+
+typedef struct {
+	uint8_t a;
+	uint8_t f;
+} Func126b3Result;
+
+Func126b3Result Func_126b3(void);
+
+#endif
--- tests/routines.py
+++ tests/routines.py
@@ -291,6 +291,7 @@ ROUTINES: dict[str, tuple[str, ...]] = {
     "duel_core_status": ("IsArenaPokemonAsleepOrPoisoned",
                          "DiscardAttachedPlusPowers", "DiscardAttachedDefenders"),
     "give_booster_pack": ("_PauseMenu_Exit",),
+    "debug_main": ("Func_126b3",),
 }
 
 EXCLUSIONS: dict[str, dict[str, dict[str, str]]] = {
