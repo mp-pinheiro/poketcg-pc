c293410340506f6a20e0f6a36052d53282b9625f
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-12 22:56:13 -0300

    fix(port): deduplicate core statics

--- src/home/core.c
+++ src/home/core.c
@@ -6,14 +6,12 @@
 /* >>> factory statics */
 #include "home/duel.h"
 
-#define DUELVARS_NUMBER_OF_POKEMON_IN_PLAY_AREA 0x1Au
+#define DUELVARS_NUMBER_OF_POKEMON_IN_PLAY_AREA 0xEFu
 #define DUELVARS_ARENA_CARD_HP                  0x08u
 #define MENU_CANCEL 0xFFu
 #define PAD_A     0x01u
 #define PAD_B     0x02u
 #define PAD_START 0x08u
-#define MENU_CANCEL 0xFFu
-#define DUELVARS_NUMBER_OF_POKEMON_IN_PLAY_AREA 0xEFu
 #define B_PAD_B_BIT 0x02u
 
 #define ASLEEP           0x02u
