782c103eb84945818b46fa464f895f311e707813
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-23 14:26:15 -0300

    fix(probe): reformat deck_machine_room.c to multi-line convention

--- src/probe/deck_machine_room.c
+++ src/probe/deck_machine_room.c
@@ -1,5 +1,27 @@
 #include "home/deck_machine_room.h"
 #include "probe.h"
-static void adapt_Func_d96c(ProbeState *s){FuncD96cResult r=Func_d96c(s->a);s->a=r.a;s->b=r.b;s->c=r.c;s->hl=r.hl;}
-static void adapt_Script_BeatAaron(ProbeState *s){(void)s;Script_BeatAaron();}
-const ProbeEntry probe_entries_deck_machine_room[]={{"Func_d96c",adapt_Func_d96c},{"Script_BeatAaron",adapt_Script_BeatAaron},{NULL,NULL}};
+
+/* >>> factory Func_d96c */
+static void adapt_Func_d96c(ProbeState *s)
+{
+	FuncD96cResult r = Func_d96c(s->a);
+	s->a = r.a;
+	s->b = r.b;
+	s->c = r.c;
+	s->hl = r.hl;
+}
+/* <<< factory Func_d96c */
+
+/* >>> factory Script_BeatAaron */
+static void adapt_Script_BeatAaron(ProbeState *s)
+{
+	(void)s;
+	Script_BeatAaron();
+}
+/* <<< factory Script_BeatAaron */
+
+const ProbeEntry probe_entries_deck_machine_room[] = {
+	{ "Func_d96c", adapt_Func_d96c },
+	{ "Script_BeatAaron", adapt_Script_BeatAaron },
+	{ NULL, NULL },
+};
