8c928f41568b78383ae3149bf0804fb3f1fb4ae4
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-26 03:34:37 -0300

    feat(oracle): report frame boundary counters

--- tools/oracle/gbref/runner.c
+++ tools/oracle/gbref/runner.c
@@ -614,6 +614,7 @@ int main(int argc, char **argv) {
     uint64_t cycles = 0;
     size_t input_index = 0;
     uint64_t frame_cycles = 0;
+    uint64_t real_boundaries = 0, synth_vblanks = 0;
     g_joypad_buttons = input_count ? input_buttons[0] : 0xff;
     g_joypad_dpad = input_count ? input_dpad[0] : 0xff;
     for (size_t i = 0; i < setup_count; i++) {
@@ -678,11 +679,14 @@ int main(int argc, char **argv) {
             fprintf(stdout, "{\"status\":\"BUDGET_EXHAUSTED\",\"pc\":%u,\"sp\":%u,"
                     "\"instructions\":%" PRIu64 ",\"cycles\":%" PRIu64 ","
                     "\"lcdc\":%u,\"if\":%u,\"ie\":%u,\"ime\":%u,\"halted\":%u,"
+                    "\"frame_cycles\":%" PRIu64 ",\"real_boundaries\":%" PRIu64 ","
+                    "\"synth_vblanks\":%" PRIu64 ","
                     "\"ppu_lcdc\":%u,\"ppu_ly\":%u,\"ppu_mode\":%u,\"bus\":\"",
                     ctx->pc, ctx->sp, steps, cycles,
                     gb_read8(ctx, 0xff40), gb_read8(ctx, 0xff0f),
                     gb_read8(ctx, 0xffff), (unsigned)(ctx->ime ? 1 : 0),
                     (unsigned)(ctx->halted ? 1 : 0),
+                    frame_cycles, real_boundaries, synth_vblanks,
                     view ? view->lcdc : 0u, view ? view->ly : 0u,
                     view ? (unsigned)view->mode : 0u);
             print_bus_spans(ctx, bus_addresses, bus_sizes, bus_count);
@@ -725,6 +729,7 @@ int main(int argc, char **argv) {
         frame_cycles += delta;
         int boundary = gb_frame_complete(ctx);
         if (boundary) {
+            real_boundaries++;
             gb_reset_frame(ctx);
         } else if (frame_cycles >= 70224u &&
                    (!(gb_read8(ctx, 0xff40) & 0x80) || ctx->halted)) {
@@ -739,6 +744,7 @@ int main(int argc, char **argv) {
             if (ctx->halted && (gb_read8(ctx, 0xff40) & 0x80))
                 gb_write8(ctx, 0xff0f,
                           (uint8_t)(gb_read8(ctx, 0xff0f) | 0x01));
+            synth_vblanks++;
         }
         if (boundary) {
             frame_cycles = 0;
