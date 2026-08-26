89cdc90f3775c7495ebe4d4fe034c3716742bdda
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-08-26 15:18:39 -0300

    chore(progress): refresh port status

--- src/home/give_booster_pack.h
+++ src/home/give_booster_pack.h
@@ -1,6 +1,8 @@
 #ifndef POKETCG_HOME_GIVE_BOOSTER_PACK_H
 #define POKETCG_HOME_GIVE_BOOSTER_PACK_H
 
+#include <stdint.h>
+
 void _PauseMenu_Exit(void);
 
 #endif /* POKETCG_HOME_GIVE_BOOSTER_PACK_H */
